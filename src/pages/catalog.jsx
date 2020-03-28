e1709d7e55fd5e6fc823908089cd7d7f806224ea
sunaryanto <hs1998281@gmail.com>
2020-03-28 18:57:56 +0700

    Update Catalog Page

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -23,14 +23,114 @@ const Render = (props) => {
                                     Kategori
                                 </h6>
                             </div>
-                            <div className="collapse" id="filterSection1">
+                            <div className="collapse show" id="filterSection1">
                                 <div className="card-body">
-                                    <p>
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high
-                                        life accusamus terry richardson ad squid.
-                                        Nihil anim keffiyeh helvetica, craft beer labore wes
-                                        anderson cred nesciunt sapiente ea proident.
-                                    </p>
+                                    <ul className="list-group">
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_category" id="cb_category_1"/>
+                                            <label htmlFor="cb_category_1">Cras justo odio</label>
+                                            <span className="badge badge-pill">14</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_category" id="cb_category_2"/>
+                                            <label htmlFor="cb_category_2">Dapibus ac facilisis in</label>
+                                            <span className="badge badge-pill">12</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_category" id="cb_category_3"/>
+                                            <label htmlFor="cb_category_3">Morbi leo risus</label>
+                                            <span className="badge badge-pill">4</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_category" id="cb_category_4"/>
+                                            <label htmlFor="cb_category_4">Porta ac consectetur ac</label>
+                                            <span className="badge badge-pill">27</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_category" id="cb_category_5"/>
+                                            <label htmlFor="cb_category_5">Vestibulum at eros</label>
+                                            <span className="badge badge-pill">2</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-header">
+                                <h6 className="mb-0"
+                                    data-toggle="collapse"
+                                    data-target="#filterSection2"
+                                    aria-expanded="false"
+                                    aria-controls="filterSection2">
+                                    Merk
+                                </h6>
+                            </div>
+                            <div className="collapse show" id="filterSection2">
+                                <div className="card-body">
+                                    <ul className="list-group">
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_merk" id="cb_merk_1"/>
+                                            <label htmlFor="cb_merk_1">Cras justo odio</label>
+                                            <span className="badge badge-pill">14</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_merk" id="cb_merk_2"/>
+                                            <label htmlFor="cb_merk_2">Dapibus ac facilisis in</label>
+                                            <span className="badge badge-pill">12</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_merk" id="cb_merk_3"/>
+                                            <label htmlFor="cb_merk_3">Morbi leo risus</label>
+                                            <span className="badge badge-pill">4</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_merk" id="cb_merk_4"/>
+                                            <label htmlFor="cb_merk_4">Porta ac consectetur ac</label>
+                                            <span className="badge badge-pill">27</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="checkbox" name="cb_merk" id="cb_merk_5"/>
+                                            <label htmlFor="cb_merk_5">Vestibulum at eros</label>
+                                            <span className="badge badge-pill">2</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-header">
+                                <h6 className="mb-0"
+                                    data-toggle="collapse"
+                                    data-target="#filterSection3"
+                                    aria-expanded="false"
+                                    aria-controls="filterSection3">
+                                    Harga
+                                </h6>
+                            </div>
+                            <div className="collapse show" id="filterSection3">
+                                <div className="card-body">
+                                    <ul className="list-group">
+                                        <li className="list-group-item">
+                                            <input type="radio" name="cb_merk" id="cb_harga_1"/>
+                                            <label htmlFor="cb_harga_1">Rp 10.000.000,00 - Rp 20.000.000,00</label>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="radio" name="cb_merk" id="cb_harga_2"/>
+                                            <label htmlFor="cb_harga_2">Rp 5.000.000,00 - Rp 10.000.000,00</label>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="radio" name="cb_merk" id="cb_harga_3"/>
+                                            <label htmlFor="cb_harga_3">Rp 1.000.000,00 - Rp 5.000.000,00</label>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="radio" name="cb_merk" id="cb_harga_4"/>
+                                            <label htmlFor="cb_harga_4">Rp 500.000,00 - Rp 1.000.000,00</label>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <input type="radio" name="cb_merk" id="cb_harga_5"/>
+                                            <label htmlFor="cb_harga_5">Rp 0 - Rp 500.000,00</label>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
